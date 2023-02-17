a5ffbf0102337ea8c5aec48196879eb7b0b73449
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-02-17 17:50:50 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Howard Thomas on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
