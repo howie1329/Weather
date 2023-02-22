c77741bc792db12905b9482e29ba2051cc530162
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-02-22 12:55:12 -0500

    Added Gauge to city view to see current weather from 0 to 100

--- Weather/View/CityView.swift
+++ Weather/View/CityView.swift
@@ -36,15 +36,21 @@ struct CityView: View {
                     VStack(spacing:10){
                         Text("\(model.currentTemp)°")
                             .font(.system(size: 60, weight: .bold))
-                        Text("Feels Like: \(model.feelsLike)°")
-                            .font(.subheadline)
-                            .bold()
-                        Text(model.description.capitalized)
-                            .bold()
-                            .font(.system(size: 30))
                         
                         Text("Min: \(model.minTemp)° Max: \(model.maxTemp)°")
                             .font(.subheadline)
+                            .bold()
+                        
+                        Gauge(value: Float(model.currentTemp) ?? 0.0, in: 0...100){}
+                            .gaugeStyle(.accessoryLinear)
+                        
+                        Text("Feels Like: \(model.feelsLike)°")
+                            .font(.subheadline)
+                            .bold()
+                        
+                        Text(model.description.capitalized)
+                            .bold()
+                            .font(.system(size: 30))
                     }
                     Spacer()
                     Button("Tap For Details"){
@@ -62,6 +68,7 @@ struct CityView: View {
                     .buttonStyle(.bordered)
                     
                 }
+                .padding()
                 .onAppear(){
                     if featuredView {
                         model.getWeather(lat: Data().cities[model.favCity]![0], long: Data().cities[model.favCity]![1])
